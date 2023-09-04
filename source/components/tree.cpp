307d58705a4e0ca62e0a8d618dcfeb0c98d73852
haiminh2001 <haiminhnguyen2001@gmail.com>
2023-09-04 16:44:53 +0700

    finish draw tree

--- source/components/tree.cpp
+++ source/components/tree.cpp
@@ -2,8 +2,8 @@
 #include <utility>
 #include <vector>
 #include <stack>
-#include <graphics.h>
-#include <iostream>
+#include <graphviz/gvc.h>
+#include <fstream>
 
 ArrayXf Tree::eval(const ArrayXXf& X){
     auto length = this->nodes.size();
@@ -66,55 +66,39 @@ Tree::Tree(std::vector<Node*> nodes){
 }
 
 void printSubTree(unsigned long x, unsigned long y, std::vector <Node*> nodes,
-                  unsigned long index)
+                  unsigned long index, std::ofstream& outputFile)
 {
     //x, y is the position of the node
 
     auto node = nodes[index];
 
-    // Set color of the boundary of
-    // circle as green
-    setcolor(GREEN);
 
-    // Draw the circle of radius 15
-    // that represent node of Tree
-    circle(x, y, 15);
-    floodfill(x, y, GREEN);
 
-    // Print the values of the node
-    // in the circle
 
-    char* node_content = const_cast<char *> (node->symbol.c_str());
+    outputFile<< "\tnode" << index << " [label=\"" << node->symbol.c_str() << "\"];" << std::endl;
 
-    outtextxy(x - 2, y - 3, node_content);
-
-    // Set the color of the line
-    // from parent to child as green
-    setcolor(GREEN);
 
     unsigned long cur_index = index - 1;
 
     for (int i = 0; i < node->arity; ++i){
         unsigned long new_x = x - y / (index + 1);
         unsigned long new_y = y + 50;
-        printSubTree(new_x, new_y, nodes, cur_index);
+        outputFile << "\tnode" << index << " -> node" << cur_index << ";" << std::endl;
+        printSubTree(new_x, new_y, nodes, cur_index, outputFile);
 
         cur_index -= nodes[cur_index]->length + 1;
-//        draw edge
-        line(x, y, new_x, new_y);
     }
 
 }
 
 void Tree::visualize(){
-    int gd = DETECT, gm;
-    auto driver = const_cast<char *> ("None");
-
-    initgraph(&gd, &gm, driver);
-    auto a =5;
-    printSubTree(300, 100, this->nodes,this->nodes.size() - 1);
-    getch();
-    closegraph();
+    std::ofstream outputFile("/tmp/tree.dot");
+    outputFile << "digraph Tree {" << std::endl;
+    printSubTree(300, 100, this->nodes,this->nodes.size() - 1, outputFile);
+    outputFile << "}" << std::endl;
+    outputFile.close();
+    std::system("dot -Tpng /tmp/tree.dot -o tree.png");
 };
+
 
 
